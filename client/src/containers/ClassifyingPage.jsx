9d6d8c98a844b750cce57b9e6ae9fcc268d0c02a
Omar Gamily <omar.e.gamily@gmail.com>
2020-10-08 18:40:20 +0200

    basic classifying page

--- client/src/containers/ClassifyingPage.jsx
+++ client/src/containers/ClassifyingPage.jsx
@@ -1,15 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styled from "styled-components";
-const Container = styled.div``
+import _ from "lodash";
+import Server from "../server";
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Controls = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Img = styled.img`
+  width: 65vw;
+`;
 class ClassifyingPage extends Component {
-    render() {
-        return (
-            <div>
-                <p>ClassifyingPage</p>
-            </div>
-        );
-    }
+  state = {};
+  //   componentDidMount() {
+  //     let projectName = this.props.projectName;
+  //     Server.requestProjectInfo(projectName)
+  //       .then(res => res.json())
+  //       .then(res => this.setState(res));
+  //   }
+  getImageUrl() {
+    return;
+  }
+  render() {
+    const { classes } = this.state;
+    return (
+      <Container>
+        <Img />
+        <Controls>
+          <div>
+            {_.map(classes, c => (
+              <input type="radio" name="classes" value={c} />
+            ))}
+          </div>
+          <div>
+            <button>Submit</button>
+            <button>discard</button>
+          </div>
+        </Controls>
+      </Container>
+    );
+  }
 }
 
 export default ClassifyingPage;
