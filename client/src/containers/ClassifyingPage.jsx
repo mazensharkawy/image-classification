1972d776cf3e8df01e282bf685a14d991169faaa
Mazen Sharkawy <mazenmaher1997@gmail.com>
2020-10-08 12:47:36 +0200

    basic ClassifyingPage

--- client/src/containers/ClassifyingPage.jsx
+++ client/src/containers/ClassifyingPage.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
+const Container = styled.div``
 
 class ClassifyingPage extends Component {
     render() {
         return (
             <div>
-                
+                <p>ClassifyingPage</p>
             </div>
         );
     }
