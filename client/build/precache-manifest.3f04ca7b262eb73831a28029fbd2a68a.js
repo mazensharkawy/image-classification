149d1ae932e9ae6374673bda6ceeca3ec8447e0f
Mazen Sharkawy <mazenmaher1997@gmail.com>
2020-10-10 23:53:14 +0200

    build and change root script file

---
+++ client/build/precache-manifest.3f04ca7b262eb73831a28029fbd2a68a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e7e8dc9bd536aadb2598efe7be2664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99abb0a3433ac803c82b",
+    "url": "/static/css/main.3187d3cb.chunk.css"
+  },
+  {
+    "revision": "97d9c085df0a7780deea",
+    "url": "/static/js/2.b507f588.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.b507f588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99abb0a3433ac803c82b",
+    "url": "/static/js/main.b5ff8b58.chunk.js"
+  },
+  {
+    "revision": "7535486f976124a8fd5c",
+    "url": "/static/js/runtime-main.54ca0fa6.js"
+  }
+]);
