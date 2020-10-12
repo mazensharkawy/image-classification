fb3e7fcf76e09cacefac8fda4b39f82ed66f5287
Mazen Sharkawy <mazenmaher1997@gmail.com>
2020-10-12 18:25:48 +0200

    serve index.html

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 import bodyParser from "body-parser";
 import express from "express";
+import path from "path";
 import routes from "./routes";
 
 const port = process.env.PORT || 3001;
 const server = express();
-
+server.use(
+  express.static(path.join(__dirname, "../client/build"), { index: false })
+);
 server.use(bodyParser.json());
 server.use("/api", routes);
-server.use(express.static('./Images'))
+server.use(express.static("./Images"));
 
-server.listen(port, (err) => {
+server.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+server.listen(port, err => {
   if (err) throw err;
   console.log(`Server listening on ${port}`);
 });
